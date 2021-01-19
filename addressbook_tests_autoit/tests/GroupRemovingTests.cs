c38516621fc92492db1f77cea38273f15766c2c0
nivanova <natalia.ivanova@IVANOVANAT10P1>
2021-01-19 11:31:29 +0100

    Add project files.

---
+++ addressbook_tests_autoit/tests/GroupRemovingTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace addressbook_tests_autoit
+{
+    public class GroupRemovingTests : TestBase
+    {
+        [Test]
+        public void TestGroupRemoving() 
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            GroupData toBeRemoved = oldGroups[0];
+            app.Groups.Remove(toBeRemoved);
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Remove(toBeRemoved);
+            oldGroups.Sort();
+            newGroups.Sort();
+
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+     }
+}
+
